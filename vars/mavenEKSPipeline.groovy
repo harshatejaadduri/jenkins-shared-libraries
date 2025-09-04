f3d3ef091c76c7f3166adeffd434174fd6770888
harshatejaadduri <harshateja3@gmail.com>
2025-09-05 00:46:28 +0530

    commented scans

--- vars/mavenEKSPipeline.groovy
+++ vars/mavenEKSPipeline.groovy
@@ -97,7 +97,7 @@ def call(Map configMap){
                         withAWS(credentials: 'aws-creds', region: 'us-east-1') {
                             sh """
                                 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
-                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
+                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} . 
                                 aws ecr create-repository --repository-name ${PROJECT}/${COMPONENT} --region ${REGION}
                                 docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
                                 """
